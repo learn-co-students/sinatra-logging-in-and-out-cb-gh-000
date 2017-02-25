e9cd275dc41e892b6838d308d7d099b4a5828af1
Hiram Cruz <hiram0101@gmail.com>
2017-02-24 19:10:51 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
+
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -10,18 +12,34 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  post '/login' do
+  get '/test' do
+    "#{User.all}"
+  end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect to('/account')
+    end
   end
 
   get '/account' do
-
+    @is_logged_in = Helpers.is_logged_in?(session)
+    if @is_logged_in
+      @current_user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to('/')
   end
 
 
 end
-
---
+++ db/migrate/20170224235152_fix_balance_datatype.rb
@@ -0,0 +1,5 @@
+class FixBalanceDatatype < ActiveRecord::Migration[5.0]
+  def change
+    change_column :users, :balance, :decimal
+  end
+end
