cb163657dc6ea7aee186364e08727a259ec5c3b4
Rauof M <abdelrauof197@gmail.com>
2018-02-21 07:38:01 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  
+  def self.current_user(sess)
+    user = User.find(sess[:user_id])
+    user
+  end
+  
+  def self.is_logged_in?(sess)
+    return true if sess[:user_id]  
+  end 
+  
 end
